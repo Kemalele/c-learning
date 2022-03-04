a18ca09a57b0abaaaa72b935a6eb561dc617f4e0
kamal.bairamov <kamal.bairamov@itechart-group.com>
2022-03-04 14:42:20 +0600

    turn on all

--- led-5161BS/led.c
+++ led-5161BS/led.c
@@ -6,23 +6,37 @@
 #include <util/delay.h>
 
 
-const uint8_t PORT12 = DDB4;
+const uint8_t BUTTON_PORT12 = DDB4;
 
-const uint8_t PORT11 = DDB3;
-const uint8_t PORT10 = DDB2;
-const uint8_t PORT9 = DDB1;
-const uint8_t PORT8 = DDB0;
+const uint8_t RIGHT_BOTTOM_PORT11 = DDB3;
+// const uint8_t PORT10 = DDB2;
+const uint8_t CENTER_BOTTOM_PORT9 = DDB1;
+const uint8_t LEFT_BOTTOM_PORT8 = DDB0;
 
-// const uint8_t PORT3 = DDA2;
-const uint8_t PORTT2 = DDD2;
+const uint8_t CENTER_CENTER_PORT2 = DDD2;
+const uint8_t LEFT_TOP_PORT3 = DDD3;
+const uint8_t CENTER_TOP_PORT4 = DDD4;
+const uint8_t RIGHT_TOP_PORT5 = DDD5;
 
-const uint8_t PIN12 = PINB4;
+const uint8_t BUTTON_PIN12 = PINB4;
 
 const uint8_t OUTPUT = 1;
 const uint8_t INPUT = 0;
 
 const uint8_t ON = 1;
 const uint8_t OFF = 0;
+void turnOffLed();
+void handleNumbers(int counter);
+void onZero();
+void onOne();
+void onTwo();
+void onThree();
+void onFour();
+void onFive();
+void onSix();
+void onSeven();
+void onEight();
+void onNine();
 
 void pinModeDDRD(uint8_t port, uint8_t mode);
 void digitalWritePIND(uint8_t port, uint8_t value);
@@ -33,35 +47,76 @@ void digitalWritePINB(uint8_t port, uint8_t value);
 uint8_t digitalReadPINB(uint8_t pin);
 
 int main() {
-  pinModeDDRD(PORTT2, OUTPUT);
+  pinModeDDRD(CENTER_CENTER_PORT2, OUTPUT);
+  pinModeDDRD(LEFT_TOP_PORT3, OUTPUT);
+  pinModeDDRD(CENTER_TOP_PORT4, OUTPUT);
+  pinModeDDRD(RIGHT_TOP_PORT5, OUTPUT);
 
-  pinModeDDRB(PORT8, OUTPUT);
-  pinModeDDRB(PORT9, OUTPUT);
-  pinModeDDRB(PORT10, OUTPUT);
-  pinModeDDRB(PORT11, OUTPUT);
-  pinModeDDRB(PORT12, INPUT);
 
+  pinModeDDRB(LEFT_BOTTOM_PORT8, OUTPUT);
+  pinModeDDRB(CENTER_BOTTOM_PORT9, OUTPUT);
+  pinModeDDRB(RIGHT_BOTTOM_PORT11, OUTPUT);
+  pinModeDDRB(BUTTON_PORT12, INPUT);
+
+  // int counter = 0;
   while(1) {
     // get value from button on pin 12
-    uint8_t buttonOn = digitalReadPINB(PIN12);
-
+    uint8_t buttonOn = digitalReadPINB(BUTTON_PIN12);
     if (buttonOn != 0) {
-      digitalWritePIND(PORTT2, OFF);
+      digitalWritePIND(CENTER_CENTER_PORT2, OFF);
+      digitalWritePIND(LEFT_TOP_PORT3, OFF);
+      digitalWritePIND(CENTER_TOP_PORT4, OFF);
+      digitalWritePIND(RIGHT_TOP_PORT5, OFF);
 
-      digitalWritePINB(PORT8, OFF);
-      digitalWritePINB(PORT9, OFF);
-      digitalWritePINB(PORT10, OFF);
-      digitalWritePINB(PORT11, OFF);
+
+      digitalWritePINB(LEFT_BOTTOM_PORT8, OFF);
+      digitalWritePINB(CENTER_BOTTOM_PORT9, OFF);
+      digitalWritePINB(RIGHT_BOTTOM_PORT11, OFF); 
+      // turnOffLed();
+      // digitalWritePIND(CENTER_CENTER_PORT2, ON);
+      // digitalWritePIND(LEFT_TOP_PORT3, ON);
+      // digitalWritePIND(CENTER_TOP_PORT4, ON);
+      // digitalWritePIND(RIGHT_TOP_PORT5, ON);
+
+
+      // digitalWritePINB(LEFT_BOTTOM_PORT8, ON);
+      // digitalWritePINB(CENTER_BOTTOM_PORT9, ON);
+      // digitalWritePINB(RIGHT_BOTTOM_PORT11, ON);       
       _delay_ms(BLINK_DELAY_MS);
-    } else { 
-      digitalWritePIND(PORTT2, ON);
+    } else {
+      digitalWritePIND(CENTER_CENTER_PORT2, ON);
+      digitalWritePIND(LEFT_TOP_PORT3, ON);
+      digitalWritePIND(CENTER_TOP_PORT4, ON);
+      digitalWritePIND(RIGHT_TOP_PORT5, ON);
 
-      digitalWritePINB(PORT8, ON);
-      digitalWritePINB(PORT9, ON);
-      digitalWritePINB(PORT10, ON);
-      digitalWritePINB(PORT11, ON); 
+
+      digitalWritePINB(LEFT_BOTTOM_PORT8, ON);
+      digitalWritePINB(CENTER_BOTTOM_PORT9, ON);
+      digitalWritePINB(RIGHT_BOTTOM_PORT11, ON); 
+
+
+      // digitalWritePIND(CENTER_CENTER_PORT2, OFF);
+      // digitalWritePIND(LEFT_TOP_PORT3, OFF);
+      // digitalWritePIND(CENTER_TOP_PORT4, OFF);
+      // digitalWritePIND(RIGHT_TOP_PORT5, OFF);
+
+
+      // digitalWritePINB(LEFT_BOTTOM_PORT8, OFF);
+      // digitalWritePINB(CENTER_BOTTOM_PORT9, OFF);
+      // digitalWritePINB(RIGHT_BOTTOM_PORT11, OFF); 
       _delay_ms(BLINK_DELAY_MS);
     }
+
+    // if (counter == 9) {
+    //   counter = 9;
+    // } else {
+    //   counter++;
+    // }
+    // turnOffLed();
+    // _delay_ms(BLINK_DELAY_MS);
+
+
+    // handleNumbers(counter);
   }
 }
 
@@ -152,3 +207,144 @@ void digitalWritePIND(uint8_t port, uint8_t value) {
 }
 
 
+void onZero() {
+  digitalWritePIND(LEFT_TOP_PORT3, ON);
+  digitalWritePIND(CENTER_TOP_PORT4, ON);
+  digitalWritePIND(RIGHT_TOP_PORT5, ON);
+
+
+  digitalWritePINB(LEFT_BOTTOM_PORT8, ON);
+  digitalWritePINB(CENTER_BOTTOM_PORT9, ON);
+  digitalWritePINB(RIGHT_BOTTOM_PORT11, ON); 
+}
+
+void onOne() {
+  digitalWritePIND(RIGHT_TOP_PORT5, ON);
+  digitalWritePINB(RIGHT_BOTTOM_PORT11, ON); 
+}
+
+void onTwo() {
+  digitalWritePIND(CENTER_CENTER_PORT2, ON);
+  digitalWritePIND(CENTER_TOP_PORT4, ON);
+  digitalWritePIND(RIGHT_TOP_PORT5, ON);
+
+
+  digitalWritePINB(LEFT_BOTTOM_PORT8, ON);
+  digitalWritePINB(CENTER_BOTTOM_PORT9, ON);
+}
+
+void onThree() {
+  digitalWritePIND(CENTER_CENTER_PORT2, ON);
+  digitalWritePIND(CENTER_TOP_PORT4, ON);
+  digitalWritePIND(RIGHT_TOP_PORT5, ON);
+
+
+  digitalWritePINB(CENTER_BOTTOM_PORT9, ON);
+  digitalWritePINB(RIGHT_BOTTOM_PORT11, ON); 
+}
+
+void onFour() {
+  digitalWritePIND(CENTER_CENTER_PORT2, ON);
+  digitalWritePIND(LEFT_TOP_PORT3, ON);
+  digitalWritePIND(RIGHT_TOP_PORT5, ON);
+  digitalWritePINB(RIGHT_BOTTOM_PORT11, ON); 
+}
+
+void onFive() {
+  digitalWritePIND(CENTER_CENTER_PORT2, ON);
+  digitalWritePIND(LEFT_TOP_PORT3, ON);
+  digitalWritePIND(CENTER_TOP_PORT4, ON);
+
+  digitalWritePINB(CENTER_BOTTOM_PORT9, ON);
+  digitalWritePINB(RIGHT_BOTTOM_PORT11, ON); 
+}
+
+void onSix() {
+  digitalWritePIND(CENTER_CENTER_PORT2, ON);
+  digitalWritePIND(LEFT_TOP_PORT3, ON);
+  digitalWritePIND(CENTER_TOP_PORT4, ON);
+
+  digitalWritePINB(LEFT_BOTTOM_PORT8, ON);
+  digitalWritePINB(CENTER_BOTTOM_PORT9, ON);
+  digitalWritePINB(RIGHT_BOTTOM_PORT11, ON); 
+}
+
+void onSeven() {
+  digitalWritePIND(CENTER_TOP_PORT4, ON);
+  digitalWritePIND(RIGHT_TOP_PORT5, ON);
+  digitalWritePINB(RIGHT_BOTTOM_PORT11, ON); 
+}
+
+void onEight() {
+  digitalWritePIND(CENTER_CENTER_PORT2, ON);
+  digitalWritePIND(LEFT_TOP_PORT3, ON);
+  digitalWritePIND(CENTER_TOP_PORT4, ON);
+  digitalWritePIND(RIGHT_TOP_PORT5, ON);
+
+
+  digitalWritePINB(LEFT_BOTTOM_PORT8, ON);
+  digitalWritePINB(CENTER_BOTTOM_PORT9, ON);
+  digitalWritePINB(RIGHT_BOTTOM_PORT11, ON); 
+}
+
+void onNine() {
+  digitalWritePIND(CENTER_CENTER_PORT2, ON);
+  digitalWritePIND(LEFT_TOP_PORT3, ON);
+  digitalWritePIND(CENTER_TOP_PORT4, ON);
+  digitalWritePIND(RIGHT_TOP_PORT5, ON);
+
+  digitalWritePINB(CENTER_BOTTOM_PORT9, ON);
+  digitalWritePINB(RIGHT_BOTTOM_PORT11, ON); 
+}
+
+void turnOffLed() {
+  digitalWritePIND(CENTER_CENTER_PORT2, OFF);
+  digitalWritePIND(LEFT_TOP_PORT3, OFF);
+  digitalWritePIND(CENTER_TOP_PORT4, OFF);
+  digitalWritePIND(RIGHT_TOP_PORT5, OFF);
+
+
+  digitalWritePINB(LEFT_BOTTOM_PORT8, OFF);
+  digitalWritePINB(CENTER_BOTTOM_PORT9, OFF);
+  digitalWritePINB(RIGHT_BOTTOM_PORT11, OFF); 
+}
+
+void handleNumbers(int counter) {
+  // turnOffLed();
+  switch (counter)
+  {
+  case 0:
+    onZero();
+    break;
+  case 1:
+    onOne();
+    break;
+  case 2:
+    onTwo();
+    break;
+  case 3:
+    onThree();
+    break;
+  case 4:
+    onFour();
+    break;
+  case 5:
+    onFive();
+    break;
+  case 6:
+    onSix();
+    break;
+  case 7:
+    onSeven();
+    break;
+  case 8:
+    onEight();
+    break;
+  case 9:
+    onNine();
+    break;
+  
+  default:
+    break;
+  }
+}
